9d1b82807c4cb11f1defc3bc69955036c745c9e9
stijnvankampen <stijnvankampenmob@gmail.com>
2015-05-20 09:57:43 +0200

    com ports

--- RobotArm/Form1.cs
+++ RobotArm/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,20 @@ namespace RobotArm
 {
     public partial class Form1 : Form
     {
+        private SerialPort serialPort;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM5";
+            serialPort.Open();
         }
     }
 }
