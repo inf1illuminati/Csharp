69f4c6877be28ce358d2799707bd99250946ecdd
stijnvankampen <stijnvankampenmob@gmail.com>
2015-05-20 10:57:14 +0200

    auto find serial port

--- RobotArm/Form1.cs
+++ RobotArm/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace RobotArm
@@ -14,6 +8,7 @@ namespace RobotArm
     public partial class Form1 : Form
     {
         private SerialPort serialPort;
+        private bool found;
 
         public Form1()
         {
@@ -22,11 +17,53 @@ namespace RobotArm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-            serialPort = new SerialPort();
+            foreach (string com in SerialPort.GetPortNames())
+            {
+                Thread t = new Thread(Check) { IsBackground = true };
+                t.Start(com);
+            }
+
+            while (!found)
+            {
+                Application.DoEvents();
+                Thread.Sleep(1);
+            }
+
+            /*serialPort = new SerialPort();
             serialPort.BaudRate = 9600;
             serialPort.PortName = "COM5";
-            serialPort.Open();
+            serialPort.Open();*/
+
+        }
+
+        private void Check(object name)
+        {
+            SerialPort p = new SerialPort((string)name, 9600);
+            bool closeme = true;
+
+            try
+            {
+                p.Open();
+
+                while (!found)
+                {
+                    if (p.ReadByte() == '0')
+                    {
+                        found = true;
+                        closeme = false;
+                        serialPort = p;
+                    }
+
+                    Thread.Sleep(1);
+                }
+
+                if(closeme) p.Close();
+
+            }
+            catch (Exception)
+            {
+                
+            }
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
